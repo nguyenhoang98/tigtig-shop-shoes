40a45d1b26868c63ab6ef28e2e761934d20edb9e
nguyenhoang98 <nguyenvanhoangg150998@gmail.com>
2020-12-13 13:08:56 +0700

    first commit

--- src/Components/FlashSales/FlashSales.js
+++ src/Components/FlashSales/FlashSales.js
@@ -9,8 +9,35 @@ var settings = {
   dots: true,
   infinite: false,
   speed: 500,
-  slidesToScroll: 4,
   slidesToShow: 4,
+  slidesToScroll: 4,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 FlashSales.propTypes = {};
 function FlashSales(props) {
